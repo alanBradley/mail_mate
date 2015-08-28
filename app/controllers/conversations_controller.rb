37fbf612e8dadb80dbca1ac2bed8478f973e0fe8
Alan Bradley <alanbradley@live.ie>
2015-08-28 16:46:29 +0100

    Added navbar, show controller show action, show page and style

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,6 +2,10 @@ class ConversationsController < ApplicationController
 	# part of devise if user not authenticated they will be sent back to sign in page
   before_action :authenticate_user!
   before_action :get_mailbox
+  before_action :get_conversation, except: [:index]
+
+  def show
+  end
 
   
   # paginate method from will_paginate + styling from bootstrap-will_paginate
@@ -13,5 +17,9 @@ class ConversationsController < ApplicationController
 
   def get_mailbox
     @mailbox ||= current_user.mailbox
+  end
+
+  def get_conversation
+  	@conversation ||= @mailbox.conversations.find(params[:id])
   end
 end
