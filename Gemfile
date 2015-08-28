407c59724d80bf0a3976cef917dd3b08923db635
Alan Bradley <alanbradley@live.ie>
2015-08-28 17:21:59 +0100

    Added gravatar, helper, modified views and conversation partial

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'devise'
 
 gem 'mailboxer'
 
+gem 'gravatar_image_tag'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
 
+	# helper for page header converted to string
 	def page_header(text)
 		content_for(:page_header) { text.to_s }
 	end
+
+	# helper for gravatar used within application
+	def gravatar_for(user, size = 30, title = user.name)
+		image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-round'
+	end
+
 end
